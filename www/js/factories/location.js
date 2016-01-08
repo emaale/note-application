7aecef2a053b7fb6e8fa41eaf04785719abddbda
Manne <manne@Mannes-MacBook-Pro.local>
2016-01-08 17:50:44 -0600

    Changed location type

--- www/js/factories/location.js
+++ www/js/factories/location.js
@@ -6,7 +6,7 @@ app.factory('location', ['$http', function($http) {
 	
 	// Success function
 	var onSuccess = function(position) {
-	    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&result_type=street_address&key=AIzaSyB19RbpY_MDENXQgby3ik_BsoaGuZEaZ3c").success(function(data) {
+	    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&result_type=locality&key=AIzaSyB19RbpY_MDENXQgby3ik_BsoaGuZEaZ3c").success(function(data) {
 	    	o.location = data.results[0].formatted_address;
 	    	o.db = o.location; // Save the location to the databinding
 	    });
