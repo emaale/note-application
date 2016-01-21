66fb5be0be7ee491f2c372c35a68b520715f2ff2
Emanuel Alenius <emanuel.alenius@gmail.com>
2016-01-21 21:44:42 +0100

    Changed the back button on the edit page, so that it takes you to the note page instead

--- www/js/controllers/navCtrl.js
+++ www/js/controllers/navCtrl.js
@@ -1,6 +1,9 @@
-app.controller('NavCtrl', ['$scope', 'notes', '$state', 'search', 'sort', function($scope, notes, $state, search, sort) {
+app.controller('NavCtrl', ['$scope', 'notes', '$state', 'search', 'sort', '$stateParams', function($scope, notes, $state, search, sort, $stateParams) {
 	// Used for searching among the notes
 	$scope.search = search;
+
+	// If we are requesting a specific note
+	if($stateParams.id) $scope.id = $stateParams.id;
 
 	// Used for sorting the notes
 	$scope.sort = sort;
