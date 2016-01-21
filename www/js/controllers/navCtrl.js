3c057284931ce47c1113cb33615b4579c5f8bd69
Emanuel Alenius <emanuel.alenius@gmail.com>
2016-01-21 23:21:49 +0100

    Added delete option in drop down menu on note and edit pages

--- www/js/controllers/navCtrl.js
+++ www/js/controllers/navCtrl.js
@@ -1,4 +1,4 @@
-app.controller('NavCtrl', ['$scope', 'notes', '$state', 'search', 'sort', '$stateParams', function($scope, notes, $state, search, sort, $stateParams) {
+app.controller('NavCtrl', ['$scope', 'notes', '$state', 'search', 'sort', '$stateParams', 'settings', 'toast', function($scope, notes, $state, search, sort, $stateParams, settings, toast) {
 	// Used for searching among the notes
 	$scope.search = search;
 
@@ -11,5 +11,30 @@ app.controller('NavCtrl', ['$scope', 'notes', '$state', 'search', 'sort', '$stat
 	// Sets what to sort by
 	$scope.sortNotes = function(sortBy) {
 		$scope.sort.options = sortBy;
+	};
+
+	// Deletes the note specififed by stateParams
+	$scope.deleteNote = function() {
+		// Get all the notes
+		$scope.notes = notes.getAll();
+
+		// Hides the clicked note
+		$scope.notes[$scope.id].deleted = true;
+
+		// If set in our settings, notify the user with a toast
+		if(settings.enableToasts) {
+			// Give user a toast, with the option to undo the actions
+			toast.setActionCb(function() {
+				$scope.notes[$scope.id].deleted = false;
+			});
+
+			toast.phrase = "Deleted";
+			toast.actionPhrase = "UNDO";
+
+			toast.show();
+		}
+
+		// Redirect user to list page after toast has been set, otherwise we can't control it
+		$state.go("sortNotes");
 	};
 }]);
