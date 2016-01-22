98f63509fa88a0e86d459d7e6085be14d1d33b62
Emanuel Alenius <emanuel.alenius@gmail.com>
2016-01-22 23:54:21 +0100

    Added error handling for location

--- www/js/controllers/settingsCtrl.js
+++ www/js/controllers/settingsCtrl.js
@@ -1,4 +1,14 @@
-app.controller('SettingsCtrl', ['$scope', 'settings', function($scope, settings) {
+app.controller('SettingsCtrl', ['$scope', 'settings', 'location', function($scope, settings, location) {
 	// Connect the settings page with the settings service, so that the settings can be used on other pages
 	$scope.settings = settings;
+
+	// Runs when the location setting changes
+	$scope.changeLocationSetting = function() {
+		// If the setting was enabled, we want to start watching the location
+		if($scope.settings.enableLocation) {
+			location.startWatch();
+		} else {
+			location.stopWatch();
+		}
+	};
 }]);
--- www/js/factories/location.js
+++ www/js/factories/location.js
@@ -1,38 +1,48 @@
-app.factory('location', ['$http', function($http) {	
+app.factory('location', ['$http', 'settings', 'toast', function($http, settings, toast) {	
 	l = {
 		location: "",
-		db: ""
+		db: "",
+		watchID: null
 	};
 	
 	// Success function
-	var onSuccess = function(position) {
-		alert('Latitude: '          + position.coords.latitude          + '\n' +
-		          'Longitude: '         + position.coords.longitude         + '\n' +
-		          'Altitude: '          + position.coords.altitude          + '\n' +
-		          'Accuracy: '          + position.coords.accuracy          + '\n' +
-		          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-		          'Heading: '           + position.coords.heading           + '\n' +
-		          'Speed: '             + position.coords.speed             + '\n' +
-		          'Timestamp: '         + position.timestamp                + '\n');
-
+	l.success = function(position) {
+	    console.log("Success");
 	    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&result_type=locality&key=AIzaSyB19RbpY_MDENXQgby3ik_BsoaGuZEaZ3c").success(function(data) {
 	    	l.location = data.results[0].formatted_address;
 	    	l.db = l.location; // Save the location to the databinding
 	    });
 	};
 
-	// Log on error
-	function onError(error) {
-	    alert('code: '    + error.code    + '\n' +
-	                      'message: ' + error.message + '\n');
-	}
-
-	// Try to get precise location using GPS, otherwise use Network based Geolocation
-	navigator.geolocation.getCurrentPosition(onSuccess, onError, { maximumAge: 3600000, timeout: 5000, enableHighAccuracy: false });
+	// Error function
+	l.error = function(error) {
+		// Make sure it's a permission error, if so, we want to help the user enable it
+		if(error.code == error.PERMISSION_DENIED) {
+			console.log("Error");
+		    // If an error occurs, we need to notify the user that the GPS is disabled
+			alert("Location disabled in the devices settings. To stop getting these messages, disable the location setting in the applications settings."); 
+		}
+	};
 	
+	l.startWatch = function() {
+		console.log("Started watching");
+		l.watchID = navigator.geolocation.watchPosition(l.success, l.error, { maximumAge: 0, timeout: 3000, enableHighAccuracy: false });
+	};
+
+	l.stopWatch = function() {
+		console.log("Stopped watching");
+		l.watchID = null;
+	};
+
+	// Resets the databinding to the location
 	l.reset = function() {
 		l.db = l.location; // Save the location to the databinding
 	};
+
+	// If GPS is enabled in the settings, start watching the location
+	if(settings.enableLocation) {
+		l.startWatch();
+	}
 
 	return l;
 }]);
