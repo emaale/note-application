d9c41568da4e23840c80252b53b9868f671df162
Emanuel Alenius <emanuel.alenius@gmail.com>
2016-01-22 05:35:30 +0100

    Added debugging alerts to location to figure out why its not working on phones with GPS disabled

--- www/js/factories/location.js
+++ www/js/factories/location.js
@@ -6,6 +6,15 @@ app.factory('location', ['$http', function($http) {
 	
 	// Success function
 	var onSuccess = function(position) {
+		alert('Latitude: '          + position.coords.latitude          + '\n' +
+		          'Longitude: '         + position.coords.longitude         + '\n' +
+		          'Altitude: '          + position.coords.altitude          + '\n' +
+		          'Accuracy: '          + position.coords.accuracy          + '\n' +
+		          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+		          'Heading: '           + position.coords.heading           + '\n' +
+		          'Speed: '             + position.coords.speed             + '\n' +
+		          'Timestamp: '         + position.timestamp                + '\n');
+		
 	    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&result_type=locality&key=AIzaSyB19RbpY_MDENXQgby3ik_BsoaGuZEaZ3c").success(function(data) {
 	    	l.location = data.results[0].formatted_address;
 	    	l.db = l.location; // Save the location to the databinding
@@ -14,7 +23,8 @@ app.factory('location', ['$http', function($http) {
 
 	// Log on error
 	function onError(error) {
-	    
+	    alert('code: '    + error.code    + '\n' +
+	                      'message: ' + error.message + '\n');
 	}
 
 	navigator.geolocation.getCurrentPosition(onSuccess, onError);
